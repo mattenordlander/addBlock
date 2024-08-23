dad52e1cb2a0e5a22501d0b3c5e2ac369f406fc6
Mathias Nordlander <mattsson.nord@gmail.com>
2024-08-23 12:31:57 +0200

    ref: More readable code

--- background.js
+++ background.js
@@ -1,39 +1,24 @@
-chrome.webNavigation.onCommitted.addListener((tab)=>{
-    //  prevent script from running when other frames load
-    if(tab.frameId === 0){
-        chrome.tabs.query({active:true, lastFocuseWindow: true}, tabs =>{
+chrome.webNavigation.onCommitted.addListener((tab) => {
+    // Prevent script from running when other frames load
+    if (tab.frameId === 0) {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            if (tabs.length === 0) return;
+            
+            let url = tabs[0].url;
+            let parseUrl = url.replace("https://", "").replace("http://", "").replace("www.", "");
+            let domain = parseUrl.split('/')[0].split('?')[0];
 
-            // Get the url of the webpage
-            let url = tabs[0].url
-            // Remove unnecessary protocol definitions and www subdomain from the UR
-            let parseUrl = url.replace("https://", "")
-            .replace("http://", "")
-            .replace("www", "")
-
-
-             // Remove path and queries e.g. linkedin.com/feed or linkedin.com?query=value
-            // We only want the base domain
-            let domain = parseUrl.slice(0, parseUrl.indexOf('/') == -1 ? parseUrl.length : parseUrl.indexOf('/'))
-            .slice(0, parseUrl.indexOf('?') == -1 ? parseUrl.length : parseUrl.indexOf('?'));
-
-            try{
-                if(domain.length <1 || domain === null || domain === undefined){
-                    return;
-                } else if (domain == "linkedin.com"){
-                    runLinkedinScript();
-                    return;
-                }
-            }catch(err){
-                throw err
+            if (domain && domain === "linkedin.com") {
+                runLinkedinScript();
             }
         });
     }
 });
 
-function runLinkedinScript(){
-    // inject script from file into the webpage
+function runLinkedinScript() {
+    // Inject script from file into the webpage
     chrome.tabs.executeScript({
         file: 'linkedin.js'
     });
-    return true
+    return true;
 }
